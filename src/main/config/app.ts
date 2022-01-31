259c93dc514224b6c7924d64c95aa876a7a36a5e
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2022-01-30 22:28:13 -0300

    feat: add cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
+import cors from 'cors'
 import setupRoutes from './routes'
 
 import express, { Express } from 'express'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  app.use(cors())
   app.use(express.json())
   setupRoutes(app)
   return app
